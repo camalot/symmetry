03948b01ad04f2d5ada63c4004ec1421c71159ea
camalot <camalot@gmail.com>
2017-05-30 21:20:18 -0500

    added missing functionf ile

---
+++ .functions/._symmetry.bash
@@ -0,0 +1,81 @@
+
+function load_prompt() {
+	if [ $# -eq 0 ]; then
+		prompt_profile='default';
+	else
+		prompt_profile=$1;
+	fi
+
+	if [ -f "$HOME/.prompts/.$prompt_profile.bash" ]; then
+		source $HOME/.prompts/.$prompt_profile.bash;
+	else
+		echo "prompt '$prompt_profile' not found"
+	fi
+}
+
+function dotfiles_platform() {
+	if command -v lsb_release > /dev/null; then
+		system=$(lsb_release -si && uname -r)
+		case $($system | awk '{print tolower($0)}') in
+			*microsoft)
+				echo "windows"
+				shift
+			;;
+			raspbian*)
+				echo "pi"
+				shift
+			;;
+			ubuntu*)
+				echo "debian"
+				shift
+			;;
+		esac
+	else
+		case $(uname -s | awk '{print tolower($0)}') in
+			darwin)
+				echo "macos"
+				shift
+			;;
+			cygwin|msys|win32)
+				echo "windows"
+				shift
+			;;
+			linux)
+				echo "linux"
+				shift
+			;;
+		esac
+	fi
+}
+
+function __load_config_files() {
+	shopt -s dotglob
+	shopt -s extglob
+
+  subdirectory="$1"
+  if [ -d "$HOME/.${subdirectory}" ]; then
+		# all non-system specific files and 'this' file
+    FILES="$HOME/.${subdirectory}/@(!(.system.*@(windows|macos|pi|linux)|._*)).bash"
+    for config_file in $FILES; do
+      if [ -f "${config_file}" ]; then
+        source $config_file
+      fi
+    done
+  fi
+}
+
+function __load_local_files() {
+	shopt -s dotglob
+	system=$(dotfiles_platform)
+	for file in $HOME/.{${system},path,bash_logout,extra}; do
+		[ -r "$file" ] && [ -f "$file" ] && source "$file";
+	done;
+}
+
+function __system_logo() {
+	if command -v screenfetch >/dev/null 2>&1; then
+	  screenfetch -E
+	elif command -v archey > /dev/null 2>&1; then
+		archey
+	fi
+}
