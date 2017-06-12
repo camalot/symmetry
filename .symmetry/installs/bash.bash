36461fd8ff8aefe103543501e7ac9df713612770
Ryan Conrad <rconrad@ILNP-094G8WN-MBs-MacBook-Pro.local>
2017-06-12 11:03:14 -0500

    set up install and configure when installing bash

--- .symmetry/installs/bash.bash
+++ .symmetry/installs/bash.bash
@@ -10,7 +10,18 @@ if command -v uname > /dev/null; then
 	esac
 fi
 
-if ! fgrep -q '/bin/bash' /etc/shells; then
-	sudo echo /bin/bash >> /etc/shells;
+if [ -f "/usr/local/bin/bash" ]; then
+	if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+		echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+	fi
+	# even if it is in the /etc/shells file, we still need to set it as the current shell
+	chsh -s /usr/local/bin/bash;
+elif [ -f "/bin/bash" ]; then
+	if ! fgrep -q '/bin/bash' /etc/shells; then
+		sudo echo /bin/bash >> /etc/shells;
+	fi
+	# even if it is in the /etc/shells file, we still need to set it as the current shell
 	sudo chsh -s /bin/bash;
+else
+	echo "Unable to locate the version of bash that was installed.";
 fi
