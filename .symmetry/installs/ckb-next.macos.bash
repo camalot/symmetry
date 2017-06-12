122aad414f3e5eb4ab6188f6110ee3f60200eeb2
Ryan Conrad <ryan.conrad@wowinc.com>
2017-06-12 15:50:03 -0500

    mac defaults

---
+++ .symmetry/installs/ckb-next.macos.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# CKB NEXT is a tool for using Corsair Mouse and/or Keyboards on macos / linux.
+# https://github.com/mattanger/ckb-next
+# CKB NEXT is a fork of CKB
+# https://github.com/ccMSC/ckb
+
+
+# https://github.com/mattanger/ckb-next/releases/download/v0.2.8/ckb-v0.2.8.pkg
