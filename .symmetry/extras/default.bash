c2c350848bb15d790b919d38ae7ea7f9e35a3cb5
Ryan Conrad <ryan.conrad@wowinc.com>
2017-06-13 09:47:54 -0500

    removed the specific config in gitconfig. added example in the extras/default

--- .symmetry/extras/default.bash
+++ .symmetry/extras/default.bash
@@ -3,3 +3,10 @@
 source $HOME/.symmetry/functions/._symmetry.bash || exit 2;
 
 __load_config_files extras;
+
+# example:
+#	git.bash >
+# 	git config --global user.name "Ryan Conrad";
+# 	git config --global user.email "fake.email@domain.com";
+# 	# this will force ssh to auth to VSTS
+# 	git config --global url."ssh://camalot@camalot.visualstudio.com/".insteadOf "https://camalot.visualstudio.com/";
