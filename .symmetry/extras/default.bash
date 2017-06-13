445ac172477d379411ae0b195bcef35cbf323f10
Ryan Conrad <ryan.conrad@wowinc.com>
2017-06-13 09:48:20 -0500

    updated example

--- .symmetry/extras/default.bash
+++ .symmetry/extras/default.bash
@@ -4,7 +4,7 @@ source $HOME/.symmetry/functions/._symmetry.bash || exit 2;
 
 __load_config_files extras;
 
-# example:
+# example of using extras files:
 #	git.bash >
 # 	git config --global user.name "Ryan Conrad";
 # 	git config --global user.email "fake.email@domain.com";
