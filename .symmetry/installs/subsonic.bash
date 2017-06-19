f0edefd155f7a3dc7906d88473db8ab3186f138e
camalot <camalot@gmail.com>
2017-06-18 21:51:51 -0500

    update

--- .symmetry/installs/subsonic.bash
+++ .symmetry/installs/subsonic.bash
@@ -4,6 +4,7 @@ _install_subsonic() {
 	local env_line="JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt\n";
 	local env_file="/etc/environment";
 	local subsonic_version="6.1.1";
+
 	sudo apt-get install oracle-java8-jdk -y;
 
 	local jvm=$(java --version);
@@ -14,15 +15,19 @@ _install_subsonic() {
 			echo "$env_line" >> "$env_file";
 		fi
 	fi
-	wget -P "$HOME/" –O "subsonic-${subsonic_version}.deb" "https://s3-eu-west-1.amazonaws.com/subsonic-public/download/subsonic-${subsonic_version}.deb";
-	sudo dpkg -i "$HOME/subsonic-${subsonic_version}.deb";
-
-	rm "$HOME/subsonic-${subsonic_version}.deb";
+	if ! command -v subsonic > /dev/null 2>&1; then
+		wget -P "$HOME/" –O "subsonic-${subsonic_version}.deb" "https://s3-eu-west-1.amazonaws.com/subsonic-public/download/subsonic-${subsonic_version}.deb";
+		sudo dpkg -i "$HOME/subsonic-${subsonic_version}.deb";
+		rm "$HOME/subsonic-${subsonic_version}.deb";
+	fi
 
 	sudo /etc/init.d/subsonic stop;
 
-	sudo adduser subsonic;
-	sudo adduser subsonic audio;
+	if ! id -u subsonic > /dev/null 2>&1; then
+		sudo adduser subsonic;
+		sudo adduser subsonic audio;
+	fi
+
 	sudo sed -e 's|SUBSONIC_USER=root|SUBSONIC_USER=subsonic|' /etc/default/subsonic > $HOME/subsonic.tmp;
 	sudo mv $HOME/subsonic.tmp /etc/default/subsonic;
 	sudo chown subsonic:subsonic /var/subsonic -Rv;
