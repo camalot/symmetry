74e2541d772dc84391ab8478bfde4da50ab39777
camalot <camalot@gmail.com>
2017-08-06 11:21:27 -0500

    update script... testing

--- .symmetry/installs/kubernetes.bash
+++ .symmetry/installs/kubernetes.bash
@@ -7,12 +7,15 @@ case $(__symmetry_platform) in
 			(>&2 echo "Docker should be installed before kubernetes");
 			exit 1;
 		fi
+		sudo apt update;
+
+		if [ "$1" -eq "reset" ]; then
+			sudo kubeadm reset;
+		fi
 
 		curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl;
 		chmod +x kubectl;
 		sudo mv kubectl /usr/local/bin/;
-
-		sudo apt-get update;
 
 		sudo apt install apt-transport-https -y;
 		curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -;
@@ -21,20 +24,23 @@ case $(__symmetry_platform) in
 		sudo apt update;
 		sudo apt install kubelet kubeadm -y;
 		echo "initializing kubernetes...";
-		ktoken_data=$(sudo kubeadm init --pod-network-cidr=192.168.0.0/16);
+
+		sudo kubeadm init --pod-network-cidr=192.168.0.0/16;
+
+		# ktoken_data=$(sudo kubeadm init --pod-network-cidr=192.168.0.0/16);
 		# kubeadm join --token cc7782.faf6b5e82250d4df 192.168.2.12:6443
-		ktoken=$(echo $ktoken_data | grep 'kubeadm join --token' | awk '{ print $4 }');
-		kmaster=$(echo $ktoken_data | grep 'kubeadm join --token' | awk '{ print $5 }');
+		#ktoken=$(echo $ktoken_data | grep 'kubeadm join --token' | awk '{ print $4 }');
+		#kmaster=$(echo $ktoken_data | grep 'kubeadm join --token' | awk '{ print $5 }');
 
 		mkdir -p $HOME/.kube;
-	  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config;
+	  sudo cp -iu /etc/kubernetes/admin.conf $HOME/.kube/config;
 	  sudo chown $(id -u):$(id -g) $HOME/.kube/config;
 
 		kubectl apply -f "http://docs.projectcalico.org/v2.3/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml";
 
 		sudo kubectl taint nodes --all node-role.kubernetes.io/master-;
 
-		echo -e "To join nodes to this cluster run:\n\tkubeadm join --token $ktoken $kmaster";
+		# echo -e "To join nodes to this cluster run:\n\tkubeadm join --token $ktoken $kmaster";
 	;;
 	*)
 		(>&2 echo "Unsupported platform: $(__symmetry_platform)");
