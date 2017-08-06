018c6995f6bb30e32b7cc35952d299c0ad4dc737
camalot <camalot@gmail.com>
2017-08-06 00:14:35 -0500

    minikube changes

--- .symmetry/completions/kubectl.bash
+++ .symmetry/completions/kubectl.bash
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
-
 # kubectl (Kubernetes CLI) completion
 
-if command -v kubectl &>/dev/null
-then
+if command -v kubectl &>/dev/null; then
   eval "$(kubectl completion bash)"
 fi
--- .symmetry/functions/system.debian.bash
+++ .symmetry/functions/system.debian.bash
@@ -8,10 +8,10 @@ function mkstart() {
 	minikube start;
 	## wait for minikube vm to start
 	printf "waiting for minikube vm to start up.";
-	mkubevm="0";
-	mcount_base="1";
-	mcount="0";
-	mcount_max=$(expr 90 / 5);
+	local mkubevm="0";
+	local mcount_base="1";
+	local mcount="0";
+	local mcount_max=$(expr 90 / 5);
 	while true; do
 		mkubevm=$((vboxmanage showvminfo "minikube" 2>/dev/null) | grep -c "running (since");
 		if [ $mkubevm -eq 1 ] || [ $mcount -ge $mcount_max ]; then
@@ -20,11 +20,11 @@ function mkstart() {
 		sleep 5;
 		printf ".";
 		mcount=$(expr $mcount + $mcount_base);
-		echo "count: $mcount";
 	done;
 	if [ $mkubevm -eq 1 ]; then
-		echo -e "\nbinding port 9090 to minikube";
-		socat tcp-listen:9090,fork tcp:$(minikube ip):30000 &
+		local mkip=$(minikube ip);
+		echo -e "\nbinding 0.0.0.0:9090 to minikube ${mkip}:30000";
+		socat tcp-listen:9090,fork tcp:${mkip}:30000 > /dev/null &
 	else
 		(>&2 echo -e "\nUnable to find minikube vm in allotted time.");
 		return;
