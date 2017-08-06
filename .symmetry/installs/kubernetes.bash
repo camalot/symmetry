5afce2dd1c447d3d0ef4b80f918d74d8ad112e41
camalot <camalot@gmail.com>
2017-08-06 11:14:06 -0500

    update script... testing

--- .symmetry/installs/kubernetes.bash
+++ .symmetry/installs/kubernetes.bash
@@ -3,12 +3,14 @@ case $(__symmetry_platform) in
 	ubuntu|debian)
 		source $HOME/.symmetry/functions/system.debian.bash;
 
-		if ! command -v docker 2>&1 /dev/null; then
+		if ! command -v docker > /dev/null; then
 			(>&2 echo "Docker should be installed before kubernetes");
 			exit 1;
 		fi
 
-		curl -sLo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/;
+		curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl;
+		chmod +x kubectl;
+		sudo mv kubectl /usr/local/bin/;
 
 		sudo apt-get update;
 
@@ -18,8 +20,21 @@ case $(__symmetry_platform) in
 
 		sudo apt update;
 		sudo apt install kubelet kubeadm -y;
+		echo "initializing kubernetes...";
+		ktoken_data=$(sudo kubeadm init --pod-network-cidr=192.168.0.0/16);
+		# kubeadm join --token cc7782.faf6b5e82250d4df 192.168.2.12:6443
+		ktoken=$(echo $ktoken_data | grep 'kubeadm join --token' | awk '{ print $4 }');
+		kmaster=$(echo $ktoken_data | grep 'kubeadm join --token' | awk '{ print $5 }');
 
+		mkdir -p $HOME/.kube;
+	  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config;
+	  sudo chown $(id -u):$(id -g) $HOME/.kube/config;
 
+		kubectl apply -f "http://docs.projectcalico.org/v2.3/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml";
+
+		sudo kubectl taint nodes --all node-role.kubernetes.io/master-;
+
+		echo -e "To join nodes to this cluster run:\n\tkubeadm join --token $ktoken $kmaster";
 	;;
 	*)
 		(>&2 echo "Unsupported platform: $(__symmetry_platform)");
--- .symmetry/installs/minikube.bash
+++ .symmetry/installs/minikube.bash
@@ -12,7 +12,9 @@ case $(__symmetry_platform) in
 		fi
 		curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/;
 
-		curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/;
+		curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl;
+		chmod +x kubectl;
+		sudo mv kubectl /usr/local/bin/;
 
 		source $HOME/.symmetry/functions/system.debian.bash;
 
