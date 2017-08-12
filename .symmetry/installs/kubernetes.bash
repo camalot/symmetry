2c7acd3219acf84cb309c136a37ce0961c671252
camalot <camalot@gmail.com>
2017-08-12 17:11:07 -0500

    just test the google auth

--- .symmetry/installs/kubernetes.bash
+++ .symmetry/installs/kubernetes.bash
@@ -50,10 +50,11 @@ function _install_kubernetes() {
 			if [ ! -z $KUBE_AUTH_GOOGLE_CLIENT_ID ]; then
 				echo "applying the google authentication to the api server configuration.";
 				sudo sed -i "/- kube-apiserver/a\    - --oidc-issuer-url=https://accounts.google.com\n    - --oidc-username-claim=email\n    - --oidc-client-id=$KUBE_AUTH_GOOGLE_CLIENT_ID" /etc/kubernetes/manifests/kube-apiserver.yaml;
-			elif [ ! -z $KUBE_BASIC_AUTH_FILE ]; then
-				echo "applying the basic authentication file to the api server configuration.";
-				sudo sed -i "/- kube-apiserver/a\    - --basic-auth-file=$KUBE_BASIC_AUTH_FILE" /etc/kubernetes/manifests/kube-apiserver.yaml;
-			if
+			fi
+			# if [ ! -z $KUBE_BASIC_AUTH_FILE ]; then
+			# 	echo "applying the basic authentication file to the api server configuration.";
+			# 	sudo sed -i "/- kube-apiserver/a\    - --basic-auth-file=$KUBE_BASIC_AUTH_FILE" /etc/kubernetes/manifests/kube-apiserver.yaml;
+			# if
 			# ktoken_data=$(sudo kubeadm init --pod-network-cidr=192.168.0.0/16);
 			# kubeadm join --token cc7782.faf6b5e82250d4df 192.168.2.12:6443
 			#ktoken=$(echo $ktoken_data | grep 'kubeadm join --token' | awk '{ print $4 }');
