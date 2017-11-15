c6f73fb6810d653a5afa420fa6ca11493cd5d85c
camalot <camalot@gmail.com>
2017-11-14 22:51:37 -0600

    check for WSL in the new path

--- .symmetry/exports/system.windows.bash
+++ .symmetry/exports/system.windows.bash
@@ -15,5 +15,12 @@ source "$HOME/.symmetry/functions/system.windows.bash";
 if command -v winenv > /dev/null 2>&1; then
 	export POWERSHELL=~/bin/powershell;
 	export LXSS_ROOT=$WIN_LOCALAPPDATA/lxss;
+	if [[ ! -d $LXSS_ROOT ]]; then
+		# this should loop all the paths found, and check rootfs/home for existance.
+		echo "check for non-legacy version of ubuntu on windows";
+		temp_paths=$(echo $WIN_LOCALAPPDATA/Packages/CanonicalGroupLimited.UbuntuonWindows_*);
+		first_ubuntu_path=$(echo "${temp_paths%% *}");
+		export LXSS_ROOT="$first_ubuntu_path/LocalState/rootfs/home;
+	fi
 	eval $(winenv);
 fi
