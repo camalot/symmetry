1c83d503522dcea4bd9aee46a6a3cb76fa14b2ce
camalot <camalot@gmail.com>
2017-11-15 19:54:46 -0600

    fix the path

--- .symmetry/exports/system.windows.bash
+++ .symmetry/exports/system.windows.bash
@@ -16,11 +16,11 @@ if command -v winenv > /dev/null 2>&1; then
 	export POWERSHELL=~/bin/powershell;
 	export LXSS_ROOT=$WIN_LOCALAPPDATA/lxss;
 	if [[ ! -d $LXSS_ROOT ]]; then
-		# this should loop all the paths found, and check rootfs/home for existance.
+		# this should loop all the paths found, and check rootfs for existance.
 		echo "check for non-legacy version of ubuntu on windows";
 		temp_paths=$(echo $WIN_LOCALAPPDATA/Packages/CanonicalGroupLimited.UbuntuonWindows_*);
 		first_ubuntu_path=$(echo "${temp_paths%% *}");
-		export LXSS_ROOT="$first_ubuntu_path/LocalState/rootfs/home;
+		export LXSS_ROOT="$first_ubuntu_path/LocalState/rootfs;
 	fi
 	eval $(winenv);
 fi
