07297ce44cfb73c43595b382700a6f824e98ecbb
camalot <camalot@gmail.com>
2017-11-22 00:47:26 -0600

    added completion for cheat

---
+++ .symmetry/completions/cheat.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+_cheatsh_complete()
+{
+	local cur prev opts
+	_get_comp_words_by_ref -n : cur
+
+	COMPREPLY=()
+	#cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	opts="$(curl -s cheat.sh/:list | sed -n '/<pre>/,/<\/pre>/p' | sed '1d;$d' )"
+
+	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	__ltrim_colon_completions "$cur"
+			return 0
+}
+complete -F _cheatsh_complete cheat
