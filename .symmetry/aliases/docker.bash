573066a3b18ff9a8932800078e5641cdde732c6c
Ryan Conrad <ryan.conrad@wowinc.com>
2017-11-28 14:33:48 -0600

    added some docker aliases

---
+++ .symmetry/aliases/docker.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e;
+
+if command -v docker > /dev/null; then
+	alias docvker=docker;
+	alias dovker=docker;
+	alias docekr=docker;
+	alias dockver=docker;
+	alias dicker=docker;
+fi
