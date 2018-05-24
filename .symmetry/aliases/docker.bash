2867fd8e5992f5537eeb16b93ea9600db924b95d
Ryan Conrad <ryan.conrad@wowinc.com>
2018-05-24 08:33:36 -0500

    added some aliases for tooling using docker

--- .symmetry/aliases/docker.bash
+++ .symmetry/aliases/docker.bash
@@ -6,4 +6,14 @@ if command -v docker > /dev/null; then
 	alias docekr=docker;
 	alias dockver=docker;
 	alias dicker=docker;
+
+
+	# some aliases that use docker
+	alias yamllint="docker run --user 0 --rm -v \$(pwd):/work -w /work -it boiyaa/yamllint:latest";
+	if command -v shellcheck > /dev/null; then
+		alias shellcheck="docker run --user 0 --rm -v \$(pwd):/work -it -w /work koalaman/shellcheck:latest";
+	fi
+	if command -v dotnet > /dev/null; then
+		alias dotnet="docker run --rm -v \$(pwd):/work --workdir /work -it microsoft/dotnet:latest dotnet";
+	fi
 fi
--- .symmetry/aliases/git.bash
+++ .symmetry/aliases/git.bash
@@ -4,3 +4,5 @@ if command -v git 2>&1 /dev/null; then
 	alias g="git";
 	alias got="git";
 fi
+
+function gi() { curl -L -s https://www.gitignore.io/api/\$@ ;}
