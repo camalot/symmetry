9efa452750c28fdf67992fe85ba9027a6e11a750
Ryan Conrad <ryan.conrad@imanage.com>
2018-06-22 09:08:38 -0500

    fixed the alias'

--- .symmetry/aliases/docker.bash
+++ .symmetry/aliases/docker.bash
@@ -10,10 +10,14 @@ if command -v docker > /dev/null; then
 
 	# some aliases that use docker
 	alias yamllint="docker run --user 0 --rm -v \$(pwd):/work -w /work -it boiyaa/yamllint:latest";
-	if command -v shellcheck > /dev/null; then
+	if ! command -v shellcheck > /dev/null; then
 		alias shellcheck="docker run --user 0 --rm -v \$(pwd):/work -it -w /work koalaman/shellcheck:latest";
 	fi
-	if command -v dotnet > /dev/null; then
+	if ! command -v dotnet > /dev/null; then
 		alias dotnet="docker run --rm -v \$(pwd):/work --workdir /work -it microsoft/dotnet:latest dotnet";
+	fi
+
+	if ! command -v pwsh > /dev/null; then
+		alias pwsh="docker run --rm -v \$(pwd):/work --workdir /work -it --entrypoint=/usr/bin/pwsh microsoft/powershell:latest";
 	fi
 fi
