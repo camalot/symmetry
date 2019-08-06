973692c41fe69b0612587047cc5c0040334b5a23
Ryan Conrad <ryan.conrad@imanage.com>
2019-08-06 13:13:05 -0500

    update go

--- .symmetry/installs/go.bash
+++ .symmetry/installs/go.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e;
 
-GO_VERSION="1.8.3";
-curl -O https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+GO_VERSION="1.12.7";
+curl -O https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
 
 tar xvf go${GO_VERSION}.linux-amd64.tar.gz
 
